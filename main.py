cc423ad297ec5fb07b744979f638342e5b053c16
saesaki <shetze7@gmail.com>
2024-09-28 00:52:34 +1000

    First message!

---
+++ main.py
@@ -0,0 +1,12 @@
+import math
+
+i=0
+while i<5:
+    x = int(input('Введите x = '))
+    y = int(input('Введите y = '))
+    R = abs(int(input('Введите R = ')))
+    if (((x+R)**2+(y-R)**2<=R**2) and (0<=y<=2*R)) or ((0<=x<=2*R) and (-R<=y<=0)):
+        print('Точка входит')
+    else:
+        print('Точка не входит')
+    i+=1
